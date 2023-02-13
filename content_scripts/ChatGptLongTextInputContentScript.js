3882129f4b7eca0c28fcd9a00b0b5d8da44e801e
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-02-12 18:29:18 -0800

    Initial Commit

---
+++ content_scripts/ChatGptLongTextInputContentScript.js
@@ -0,0 +1,105 @@
+
+(function () {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+  url = window.location.href;
+  let cancel=false;
+
+  const stopGeneratingButtonClassString = "btn flex justify-center gap-2 btn-neutral border-0";
+  const sendMessageButtonClassString = "absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1";
+
+  async function sendMessages(message) {
+    subStrings = splitString(message.textToImport,2000);
+    for (var i = 0; i < subStrings.length; i++) {
+      var element = subStrings[i];
+      var stringToSend = message.secondMessage + "\n\n" + element;
+      if(cancel)break;
+      await waitForRegenerateResponseButton(sendChatGPTMessage, stringToSend);
+    }
+    cancel=false;
+  }
+
+  function checkForCancel(){
+    return cancel;
+  }
+
+  function sendChatGPTMessage(messageText){
+      if(document.getElementsByTagName("textarea")[0]===undefined)return;
+      document.body.getElementsByTagName("textarea")[0].value = messageText;
+      sendMessageButtonClick();
+  }
+
+  function run(message) {
+    if (url.match("https:\/\/chat.openai.com\/chat\S*")) {
+      sendChatGPTMessage(message.firstMessage);
+      waitForRegenerateResponseButton(sendMessages, message);
+    } else {
+      console.log("Wrong Url");
+    }
+  }
+
+  function splitString(str, maxLength) {
+    let words = str.split(/\s+/);
+    let substrings = [];
+    let currentString = "";
+
+    for (let word of words) {
+      if (currentString.length + word.length <= maxLength) {
+        currentString += `${word} `;
+      } else {
+        substrings.push(currentString.trim());
+        currentString = `${word} `;
+      }
+    }
+
+    if (currentString.trim().length) {
+      substrings.push(currentString.trim());
+    }
+
+    return substrings;
+  }
+
+  function sendMessageButtonClick() {
+    document.body.getElementsByClassName(sendMessageButtonClassString)[0].click();
+  }
+
+
+  function waitForRegenerateResponseButton(callback, param1) {
+    isReady = false;
+    let buttons = document.querySelectorAll('button');
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i].textContent === "Regenerate response") {
+        isReady = true;
+        break;
+      }
+    }
+    if(cancel){
+      return;
+    }
+    else if (isReady) {
+      callback(param1);
+    } else {
+      setTimeout(() => {
+        waitForRegenerateResponseButton(callback, param1);
+      }, 100);
+    }
+  }
+
+
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "run") {
+      cancel=false;
+      run(message);
+    }else if(message.command==="stop"){
+      cancel=true;
+    }
+  });
+
+})();
---
+++ popup/ChatGptLongTextInput.js
@@ -0,0 +1,95 @@
+//get default values from html file because somehow that felt like less work than just extracting them out into here.
+var defaultValues = {
+        textToImport: document.body.getElementsByTagName("textarea")[0].value,
+        firstMessage: document.body.getElementsByTagName("input")[0].value,
+        secondMessage: document.body.getElementsByTagName("input")[1].value,
+        textToImportHeight: document.body.getElementsByTagName("textArea")[0].getAttribute("height"),
+}
+
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+
+
+    function run(tabs) {
+      browser.tabs.sendMessage(tabs[0].id, {
+        command: "run",
+        textToImport: document.body.getElementsByTagName("textarea")[0].value,
+        firstMessage: document.body.getElementsByTagName("input")[0].value,
+        secondMessage: document.body.getElementsByTagName("input")[1].value,
+      });
+    }
+
+    function reset(tabs){
+        document.body.getElementsByTagName("textarea")[0].value=defaultValues.textToImport;
+        document.body.getElementsByTagName("input")[0].value=defaultValues.firstMessage;
+        document.body.getElementsByTagName("input")[1].value=defaultValues.secondMessage;
+        document.body.getElementsByTagName("textArea")[0].setAttribute("height",defaultValues.textToImportHeight)
+      browser.tabs.sendMessage(tabs[0].id, {
+        command: "stop",
+      });
+      
+    }
+
+    /**
+     * Just log the error to the console.
+     */
+    function reportError(error) {
+      console.error(`Nicole Error: ${error}`);
+    }
+
+    if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+      // Ignore when click is not on a button within <div id="popup-content">.
+      return;
+    }
+    if (e.target.type === "reset") {
+      browser.tabs.query({ active: true, currentWindow: true })
+        .then(reset)
+        .catch(reportError);
+    } else {
+      browser.tabs.query({ active: true, currentWindow: true })
+        .then(run)
+        .catch(reportError);
+    }
+  });
+
+  window.addEventListener("unload", (event) => {
+    var data = {
+      textToImport: document.body.getElementsByTagName("textarea")[0].value,
+      firstMessage: document.body.getElementsByTagName("input")[0].value,
+      secondMessage: document.body.getElementsByTagName("input")[1].value,
+      textToImportHeight: document.body.getElementsByTagName("textArea")[0].getAttribute("height"),
+    };
+    localStorage.setItem("popupData", JSON.stringify(data));
+  });
+
+  var storedData = JSON.parse(localStorage.getItem("popupData"));
+  if (storedData !== null) {
+    document.body.getElementsByTagName("textarea")[0].value = storedData.textToImport;
+    document.body.getElementsByTagName("input")[0].value = storedData.firstMessage;
+    document.body.getElementsByTagName("input")[1].value = storedData.secondMessage;
+  }
+}
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute content script: ${error.message}`);
+}
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+
+browser.tabs.executeScript({ file: "/content_scripts/ChatGptLongTextInputContentScript.js" })
+  .then(listenForClicks)
+  .catch(reportExecuteScriptError);
