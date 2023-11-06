8432ac6d665d0d65b7ec9f05b8508deb1b7a9d6c
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-11-05 22:00:06 -0800

    Updated method to preserve whitespace

--- content_scripts/ChatGptLongTextInputSharedMethods.js
+++ content_scripts/ChatGptLongTextInputSharedMethods.js
@@ -7,40 +7,40 @@
    * @param {number} maxLength - The maximum length of each substring.
    * @returns {string[]} An array of substrings.
    */
-  function splitString(str, maxLength) {
-    let regex = /\s+/;
+function splitString(str, maxLength) {
+  let regex = /(?<=\S)(?=\s|$)/;
 
-    // Split the input string into words by the chosen regex.
-    let words = str.split(regex);
+  // Split the input string into words by the chosen regex while keeping the trailing spaces.
+  let words = str.split(regex);
 
-    // Initialize an array to hold the substrings that we will return.
-    let substrings = [];
+  // Initialize an array to hold the substrings that we will return.
+  let substrings = [];
 
-    // Initialize a string to accumulate words until we reach the maxLength.
-    let currentString = "";
+  // Initialize a string to accumulate words until we reach the maxLength.
+  let currentString = "";
 
-    // Loop over each word in the words array.
-    for (let word of words) {
-      // Check if the current word can fit into the currentString without exceeding the maxLength.
-      if (currentString.length + word.length <= maxLength) {
-        // If it fits, add the word to the currentString with a trailing space.
-        currentString += `${word} `;
-      } else {
-        // If it doesn't fit, push the currentString into the substrings array after trimming any trailing spaces.
-        substrings.push(currentString.trim());
-
-        // Start a new currentString with the current word.
-        currentString = `${word} `;
+  // Loop over each word in the words array.
+  for (let word of words) {
+    // Here we add 1 for the space that would be between the words in the substring
+    if (currentString.length + word.length + (currentString.length > 0 ? 1 : 0) <= maxLength) {
+      // Add a space if currentString is not empty and append the word.
+      currentString += (currentString.length > 0 ? " " : "") + word;
+    } else {
+      // If it doesn't fit, push the currentString into the substrings array.
+      if (currentString.length > 0) {
+        substrings.push(currentString);
       }
+      // Start a new currentString with the current word.
+      currentString = word;
     }
-
-    // After the loop, there may be a leftover currentString that hasn't been added to the substrings array.
-    // If this string is not empty (after trimming), add it to the substrings array.
-    if (currentString.trim().length) {
-      substrings.push(currentString.trim());
-    }
-
-    // Return the array of substrings.
-    return substrings;
   }
+
+  // After the loop, push the last currentString if it is not empty.
+  if (currentString.length) {
+    substrings.push(currentString);
+  }
+
+  // Return the array of substrings.
+  return substrings;
+}
 
